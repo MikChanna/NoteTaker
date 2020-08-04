5d49913f2be4ff4573cdffc7035a9d57a9cea5b6
MikChanna <channa.mik@gmail.com>
2020-08-04 14:15:41 -0400

    required dependencies and added PORT

---
+++ db.js
@@ -0,0 +1,31 @@
+// Dependencies & files
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App & Path
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "index.html");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/add", function (req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
