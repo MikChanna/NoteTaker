ed31d8ca6b7443e41f1a900ee7625714ab30f257
MikChanna <channa.mik@gmail.com>
2020-08-10 14:09:37 -0400

    add require fs

--- db.js
+++ db.js
@@ -8,9 +8,6 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "index.html");
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
