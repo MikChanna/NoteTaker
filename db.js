2ef6f21e20d15c2fbc8d61f40bae4e691050afe9
MikChanna <channa.mik@gmail.com>
2020-08-10 15:09:15 -0400

    added express.static

--- db.js
+++ db.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 // Sets up the Express App & Path
 // =============================================================
@@ -11,18 +12,29 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+// Notes DATA -
+// ==============================================================
+
+var notes = [];
 
 // Routes
 // =============================================================
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
+});
+
+// Displays all notes
+app.get("/api/notes", function (req, res) {
+  return res.json(notes);
 });
